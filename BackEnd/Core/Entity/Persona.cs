ed5f886b547bae838853984bddea14ee05c99e27
adrianalvarez <adrianalvarezcardentey@gmail.com>
2023-07-24 09:47:14 -0400

    ewfewfef

--- BackEnd/Core/Entity/Persona.cs
+++ BackEnd/Core/Entity/Persona.cs
@@ -10,7 +10,7 @@ namespace Core
         public string Nombre { get; set; }
         public DateTime FechaNacimiento { get; set; }
         public string Email { get; set; }
-		public string Develop_no { get; set; }
-		public string Master { get; set; }
+		public string Develop_SI { get; set; }
+		public string Master_no { get; set; }
     }
 }
